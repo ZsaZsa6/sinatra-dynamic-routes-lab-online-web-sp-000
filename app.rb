82135435e505bdf8c8c6f57412e428fbdfdf0393
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-24 15:05:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,9 @@ class App < Sinatra::Base
 
   end
   get '/say/:number/:phrase' do
-    @phrasenumber = params[:number].to_i.times{[:phrase]}
+    @number = params[:number].to_i
+    @phrase_number = params[:phrase]
+    @number.times do "#{@phrase_number}"
     binding.pry
   end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
