afd30c5038685367aecec79b5a289cc1732386e2
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-24 15:54:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @string = ""
-    @number.times{@string += @phrase}
+    @number.times{@string << @phrase}
 
 
     # binding.pry
